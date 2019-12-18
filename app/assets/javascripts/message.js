a1bc7b4ee721fdfb3eb6f89bae775982748beac7
hoshiiiiko <57692673+hoshiiiiko@users.noreply.github.com>
2019-12-18 22:53:04 +0900

    JSONをHTMLにして、appendで画面に表示

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,37 @@
 $(function() {
+  function buildHTML(message) {
+    if (message.image) {
+      var html = `<div class="main-contents__post">
+                    <div class="main-contents__post--name">
+                    ${message.user_name}
+                    </div>
+                    <div class="main-contents__post--date">
+                    ${message.created_at}
+                    </div>
+                  </div>
+                  <div class="main-contents__comment">
+                    <p class="main-contents__comment--sentence">
+                    ${message.content}
+                    </p>
+                    <img class="main-contents__comment--image" src="${message.image}" alt="Images">
+                  </div>`
+    } else {
+      var html = `<div class="main-contents__post">
+                    <div class="main-contents__post--name">
+                    ${message.user_name}
+                    </div>
+                    <div class="main-contents__post--date">
+                    ${message.created_at}
+                    </div>
+                  </div>
+                  <div class="main-contents__comment">
+                    <p class="main-contents__comment--sentence">
+                    ${message.content}
+                    </p>
+                  </div>`
+      }
+      return html;
+  }
   $("#new_message").on('submit',function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -9,6 +42,13 @@ $(function() {
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(message) {
+      var html = buildHTML(message);
+      $('.main-contents').append(html);
+      $('#message-content').val('');
+    })
+    .fail(function() {
     })
   })
 });
