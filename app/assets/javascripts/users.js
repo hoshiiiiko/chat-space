5ed9c197a112b153fd4baa32a468e722d7411462
hoshiiiiko <57692673+hoshiiiiko@users.noreply.github.com>
2019-12-26 14:30:04 +0900

    検索結果の表示

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,6 +1,44 @@
 $(function(){
+
+  function appendUser(user) {
+    let html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                </div>
+                `;
+    $('#chat-group-users').append(html);
+  }
+
+  function appendNoUser() {
+    let html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">ユーザーが見つかりません</p>
+                </div>
+                `;
+    $('#chat-group-users').append(html);
+  }
+
   $("#user-search-field").on('keyup',function(){
     var input = $("#user-search-field").val();
-    console.log(input);
-  })
-})
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      dataType: 'json',
+      data: { keyword: input }
+    })
+      .done(function(users){
+        $('#chat-group-users').empty();
+        if (users.length !== 0) {
+          users.forEach(function(user){
+            appendUser(user);
+          });
+        } else if (input.length == 0) {
+          return false;
+        } else {
+          appendNoUser();
+        }
+      })
+      .fail(function(){
+        alert("ユーザー検索に失敗しました");
+      });
+  });
+});
